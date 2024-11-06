2c7a27928f8053fc9648a1e9ccbe8cadab8034aa
Ranji6079 <162511625+Ranji6079@users.noreply.github.com>
2024-11-06 23:03:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -534,4 +534,4 @@ def install_libraries(libraries):
             print(f"Error installing {library}: {e}")
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=5000)
