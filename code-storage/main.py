eec4ec42da88f35940b37b2b966dde9476485631
Ranjith Kumar <Invikareddy02@gmail.com>
2024-11-14 00:36:03 +0300

    initial commit

---
+++ code-storage/main.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression
+
+
+
+data = pd.read_csv('datasets/data.csv')
+
+
+
+X = data[['X']]
+
+Y = data['Y']
+
+
+
+model = LinearRegression()
+
+
+
+model.fit(X, Y)
+
+
+
+Y_pred = model.predict(X)
+
+
+
+plt.scatter(X, Y, color='blue', label='Actual data')
+
+plt.plot(X, Y_pred, color='red', label='Linear regression line')
+
+plt.xlabel('X')
+
+plt.ylabel('Y')
+
+plt.title('Linear Regression')
+
+plt.legend()
+
+plt.show()
+
+
+
+print(f"Coefficient: {model.coef_[0]}")
+
+print(f"Intercept: {model.intercept_}")
+
